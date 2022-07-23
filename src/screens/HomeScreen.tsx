f8ffb4b8553783999f3cae01658fde74bb365408
= <=>
2022-07-22 21:33:58 -0500

    Fondo Pokebola

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Image, Text, View} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {appStyle} from '../theme/appTheme';
 
 export const HomeScreen = () => {
+  const {top} = useSafeAreaInsets();
+
   return (
-    <View>
-      <Text>PokemonScreen</Text>
-    </View>
+    <>
+      <Image
+        source={require('../assets/pokebola.png')}
+        style={appStyle.pokebolaBG}
+      />
+      <Text style={[appStyle.title, appStyle.globalMargin, {top: top + 20}]}>
+        Pokedex
+      </Text>
+    </>
   );
 };
